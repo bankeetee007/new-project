ed345fb6015ccd778ea740b217eeb8304bfa99e2
bankeetee007 <bankeetee007@gmail.com>
2025-08-09 03:36:03 +0100

    main.py

---
+++ src/main.py
@@ -0,0 +1,6 @@
+this is a very great way to start my devops career 
+and the way it seems, i am really learning fast
+i am so happy to join this line
+and i know i will enjoy it so far i am not sleeping on it
+thanks to the brother God gave to me
+he is very kind and a loving brother
